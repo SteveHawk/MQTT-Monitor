9db9c05f845a114bd85984fbf94a485065d43b91
Steve <stevehawk327@gmail.com>
2026-02-24 02:40:40 +0800

    Add fasthtml dep

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from mqtt-monitor!")
+from fasthtml.common import FT, A, Body, Div, FastHTML, Head, Html, Img, Title, serve
+
+app = FastHTML()
+
+
+@app.get("/")  # type: ignore
+def home() -> FT:
+    page = Html(
+        Head(Title("Some page")),
+        Body(
+            Div(
+                "Some text, ",
+                A("A link", href="https://example.com"),
+                Img(src="https://placehold.co/200"),
+                cls="myclass",
+            )
+        ),
+    )
+    return page
 
 
 if __name__ == "__main__":
-    main()
+    serve()
