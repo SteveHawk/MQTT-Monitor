ce8d1eef0ff205bf0593f6e5d021dcb57bf55629
Steve <stevehawk327@gmail.com>
2026-03-05 19:06:34 +0800

    implement sse triggered update to ensure consistency

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from typing import AsyncGenerator
 
 import fasthtml.common as ft
 from fasthtml.common import Article, Div, Script, Titled
-from loguru import logger
 from starlette.applications import Starlette
 from starlette.responses import StreamingResponse
 
@@ -21,7 +20,13 @@ async def mqttc_lifespan(app: Starlette) -> AsyncGenerator[None, None]:
 
 app, rt = ft.fast_app(
     lifespan=mqttc_lifespan,
-    hdrs=(Script(src="https://unpkg.com/htmx-ext-sse@2.2.1/sse.js"),),
+    hdrs=(
+        Script(src="https://unpkg.com/htmx-ext-sse@2.2.1/sse.js"),
+        Script(
+            'function getMsgId() { var msgs = [...document.querySelectorAll("#messages [id]")];'
+            'return msgs.length === 0 ? 0 : msgs.pop().id.split("_")[1]; }'
+        ),
+    ),
 )
 
 
@@ -30,32 +35,41 @@ def home() -> ft.FT:
     return Titled(
         "Meshtastic MQTT Monitor",
         Div(
+            Div(
+                *[
+                    Article(str(message), id=f"message_{message.id}")
+                    for message in mqtt_monitor.ring_buffer.fetch_all()
+                ],
+                id="messages",
+                hx_get="/fetch-messages",
+                hx_trigger="sse:new_message",
+                hx_vals="js:{current_id: getMsgId()}",
+                hx_swap="beforeend show:bottom",
+            ),
             hx_ext="sse",
-            sse_connect="/new-message",
-            hx_swap="beforeend show:bottom",
-            sse_swap="message",
-            *[
-                Article(str(message))
-                for message in mqtt_monitor.ring_buffer.fetch_all()
-            ],
+            sse_connect="/sse-new-msg",
         ),
     )
 
 
-@app.get("/new-message")
+@app.get("/sse-new-msg")
 async def new_message() -> StreamingResponse:
-    async def get_message() -> AsyncGenerator[str, None]:
+    async def notify() -> AsyncGenerator[str, None]:
         shutdown_event = ft.signal_shutdown()
-        current_id = mqtt_monitor.ring_buffer.max_id
 
         while not shutdown_event.is_set():  # TODO: handle shutdown event
             await asyncio.to_thread(mqtt_monitor.ring_buffer.wait)
-            for message in mqtt_monitor.ring_buffer.fetch_new(current_id):
-                logger.info(f"New message sent: {message.id=}, {current_id=}")
-                yield ft.sse_message(Article(str(message)))
-                current_id = message.id
+            yield ft.sse_message("new msg", event="new_message")
 
-    return StreamingResponse(get_message(), media_type="text/event-stream")
+    return StreamingResponse(notify(), media_type="text/event-stream")
+
+
+@app.get("/fetch-messages")
+def fetch_messages(current_id: int) -> list[ft.FT]:
+    return [
+        Article(str(message), id=f"message_{message.id}")
+        for message in mqtt_monitor.ring_buffer.fetch_new(current_id)
+    ]
 
 
 if __name__ == "__main__":
