6959a4887f1457135d821b674dbc49c5faa95ad4
Steve <stevehawk327@gmail.com>
2026-03-20 04:48:54 +0800

    update test to use more fixture

--- tests/test_sqlite.py
+++ tests/test_sqlite.py
@@ -45,9 +45,11 @@ def test_nodeinfo(sql_store: SQLiteStore) -> None:
     assert sql_store.fetch_nodedb()[node_num] == node_info
 
 
-def test_packets(sql_store: SQLiteStore) -> None:
+@pytest.fixture
+def sql_store_packets() -> tuple[list[Packet], list[Packet]]:
     packets = list[Packet]()
     messages = list[Packet]()
+
     for i in range(1, 20, 2):
         p = Packet(
             i,
@@ -64,9 +66,8 @@ def test_packets(sql_store: SQLiteStore) -> None:
             },
             False,
             False,
-            int(time.time()),
+            int(time.time()) - (10 - i) * 86401,
         )
-        sql_store.insert_packet(p)
         packets.append(p)
 
         p = Packet(
@@ -84,44 +85,66 @@ def test_packets(sql_store: SQLiteStore) -> None:
             },
             False,
             False,
-            int(time.time()),
+            int(time.time()) - (10 - i) * 86401,
         )
-        sql_store.insert_packet(p)
         packets.append(p)
         messages.append(p)
 
-    assert packets == sql_store.fetch_new_packets(0)
-    assert packets != sql_store.fetch_new_packets(1)
-    assert packets[7:] == sql_store.fetch_new_packets(7)
+    return packets, messages
 
-    assert packets == sql_store.fetch_old_packets(21, 20)
-    assert packets == sql_store.fetch_old_packets(25, 10000)
-    assert packets != sql_store.fetch_old_packets(20, 20)
-    assert packets != sql_store.fetch_old_packets(21, 19)
-    assert packets[7:15] == sql_store.fetch_old_packets(16, 8)
-    assert packets[:12] == sql_store.fetch_old_packets(13, 8888)
-    assert packets[-1:] == sql_store.fetch_old_packets(13333, 1)
 
-    assert packets == sql_store.fetch_latest_packets(20)
-    assert packets == sql_store.fetch_latest_packets(500)
-    assert packets != sql_store.fetch_latest_packets(19)
-    assert packets[-7:] == sql_store.fetch_latest_packets(7)
-    assert packets[-1:] == sql_store.fetch_latest_packets(1)
+@pytest.fixture
+def sql_store_full(
+    sql_store: SQLiteStore, sql_store_packets: tuple[list[Packet], list[Packet]]
+) -> SQLiteStore:
+    packets, _ = sql_store_packets
+    for p in packets:
+        sql_store.insert_packet(p)
+    return sql_store
 
-    assert messages == sql_store.fetch_new_messages(0)
-    assert messages != sql_store.fetch_new_messages(1)
-    assert messages[10:] == sql_store.fetch_new_messages(10)
 
-    assert messages == sql_store.fetch_old_messages(11, 10)
-    assert messages == sql_store.fetch_old_messages(19, 1000)
-    assert messages != sql_store.fetch_old_messages(10, 10)
-    assert messages != sql_store.fetch_old_messages(11, 9)
-    assert messages[1:7] == sql_store.fetch_old_messages(8, 6)
-    assert messages[:2] == sql_store.fetch_old_messages(3, 20)
-    assert messages[-1:] == sql_store.fetch_old_messages(2000, 1)
+def test_packets(
+    sql_store_full: SQLiteStore, sql_store_packets: tuple[list[Packet], list[Packet]]
+) -> None:
+    packets, messages = sql_store_packets
 
-    assert messages == sql_store.fetch_latest_messages(10)
-    assert messages == sql_store.fetch_latest_messages(10000)
-    assert messages != sql_store.fetch_latest_messages(9)
-    assert messages[-5:] == sql_store.fetch_latest_messages(5)
-    assert messages[-1:] == sql_store.fetch_latest_messages(1)
+    assert packets == sql_store_full.fetch_new_packets(0)
+    assert packets != sql_store_full.fetch_new_packets(1)
+    assert packets[7:] == sql_store_full.fetch_new_packets(7)
+
+    assert packets == sql_store_full.fetch_old_packets(21, 20)
+    assert packets == sql_store_full.fetch_old_packets(25, 10000)
+    assert packets != sql_store_full.fetch_old_packets(20, 20)
+    assert packets != sql_store_full.fetch_old_packets(21, 19)
+    assert packets[7:15] == sql_store_full.fetch_old_packets(16, 8)
+    assert packets[:12] == sql_store_full.fetch_old_packets(13, 8888)
+    assert packets[-1:] == sql_store_full.fetch_old_packets(13333, 1)
+
+    assert packets == sql_store_full.fetch_latest_packets(20)
+    assert packets == sql_store_full.fetch_latest_packets(500)
+    assert packets != sql_store_full.fetch_latest_packets(19)
+    assert packets[-7:] == sql_store_full.fetch_latest_packets(7)
+    assert packets[-1:] == sql_store_full.fetch_latest_packets(1)
+
+    assert messages == sql_store_full.fetch_new_messages(0)
+    assert messages != sql_store_full.fetch_new_messages(1)
+    assert messages[10:] == sql_store_full.fetch_new_messages(10)
+
+    assert messages == sql_store_full.fetch_old_messages(11, 10)
+    assert messages == sql_store_full.fetch_old_messages(19, 1000)
+    assert messages != sql_store_full.fetch_old_messages(10, 10)
+    assert messages != sql_store_full.fetch_old_messages(11, 9)
+    assert messages[1:7] == sql_store_full.fetch_old_messages(8, 6)
+    assert messages[:2] == sql_store_full.fetch_old_messages(3, 20)
+    assert messages[-1:] == sql_store_full.fetch_old_messages(2000, 1)
+
+    assert messages == sql_store_full.fetch_latest_messages(10)
+    assert messages == sql_store_full.fetch_latest_messages(10000)
+    assert messages != sql_store_full.fetch_latest_messages(9)
+    assert messages[-5:] == sql_store_full.fetch_latest_messages(5)
+    assert messages[-1:] == sql_store_full.fetch_latest_messages(1)
+
+
+def test_packets_cleanup(sql_store_full: SQLiteStore) -> None:
+    sql_store_full.cleanup_packets(10, 100)
+    sql_store_full.cleanup_messages(10, 100)
